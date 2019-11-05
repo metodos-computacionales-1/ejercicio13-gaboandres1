79dd92f032c969a7f8ae328630d29643746cad2c
Default user <ga.perezg1@uniandes.edu.co>
2019-11-05 21:44:35 +0000

    Termino clase

---
+++ ejercicio3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+long fibonacci(int n)
+{
+    if (a>1)
+        return (a*factorial(a-1));
+    else
+        return 1;
+}
+
+int main(void)
+{
+    int number=9;
+    z=addition(x,y);
+    std::cout<< number<<"! = " <<factorial(number)<<"\n";
+    return 0;
+}
