e9a4be509cd9151de329abd040d28145ae76a289
Default user <gaboandrez99@gmail.com>
2019-11-11 20:03:56 +0000

    Ejercicio 3. Verisón final

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
-long fibonacci(int n)
+long fibonacciR(long n)
 {
-    if (a>1)
-        return (a*factorial(a-1));
-    else
+    if (n == 1)
         return 1;
+    else if( n == 2)
+        return 1;
+    else
+        return fibonacciR(n-1) + fibonacciR(n-2);
+}
+long fibonacciF(long n)
+{
+    long anterior1 = 1;
+    long anterior2 = 1;
+    long actual=0;
+    for(int i=0; i<n-2; i++){
+        actual = anterior1 + anterior2;
+        anterior2 = anterior1;
+        anterior1 = actual;
+    }
+    return actual;
 }
 
 int main(void)
 {
-    int number=9;
-    z=addition(x,y);
-    std::cout<< number<<"! = " <<factorial(number)<<"\n";
+    int number=50;
+    std::cout<<"Recursivo para n="<<number<<": " <<fibonacciR(number)<<"\n";
+    std::cout<<"No recursivo para n="<<number<<": " <<fibonacciF(number)<<"\n";
     return 0;
 }
