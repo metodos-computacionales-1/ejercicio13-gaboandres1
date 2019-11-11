2775047ecbf39f7905127fd719385b3d616febed
Default user <gaboandrez99@gmail.com>
2019-11-11 19:20:26 +0000

    Ejercicio 1. Versión final

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -2,6 +2,7 @@
 int subtraction(int a, int b)
 {
     int r;
+    std::cout << "La variable local r está en la dirección de memoria: "<< &r << std::endl;
     r=a-b;
     return r;
 }
@@ -11,9 +12,12 @@ int main(void)
     int x=5,y=3,z;
     z=subtraction(7,2);
     std::cout<< "The first result is " <<z<<"\n";
-    std::cout<< "z is in memory adress: " <<&z<<"\n";
-    std::cout<< "The second result is " <<subtraction(7,2)<<"\n";
-    std::cout<< "The third result is " <<subtraction(x,y)<<"\n";
+    std::cout<< "x está en la dirección de memoria: " <<&x<<"\n";
+    std::cout<< "y está en la dirección de memoria: " <<&y<<"\n";
+    std::cout<< "z está en la dirección de memoria: " <<&z<<"\n";
+    
+    subtraction(7,2);
+    subtraction(x,y);
     z=4+subtraction(x,y);
     std::cout<< "The fourth result is " <<z<<"\n";
     std::cout<< "z is in memory adress: " <<&z<<"\n";
